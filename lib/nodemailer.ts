8b6befbcd59fb60604720bfeb37855a2b3a44474
tejas giri <95345884+tejasg910@users.noreply.github.com>
2025-11-12 17:16:52 +0530

    fixed email issue

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -4,6 +4,9 @@ export const createTransporter = (smtpUser: string, smtpPass: string) => {
     return nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: parseInt(process.env.SMTP_PORT!),
+         tls: {
+    rejectUnauthorized: false
+}, 
         auth: {
             user: smtpUser,
             pass: smtpPass
@@ -16,15 +19,17 @@ export const createTransporter = (smtpUser: string, smtpPass: string) => {
 
 export async function verifySmtpCredentials(email: string, password: string): Promise<boolean> {
   const transporter = createTransport({
-    host: 'smtp.gmail.com', // or your SMTP host
-    port: 587,
+      host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT!),
     secure: false,
+    tls: {
+    rejectUnauthorized: false
+}, 
     auth: {
       user: email,
       pass: password,
     },
-    connectionTimeout: 5000, // 5 seconds connection timeout
-    socketTimeout: 5000,     // 5 seconds socket timeout
+   
   });
 
   try {
